da276735b1a4eeb068c0960784185328ff67fc0a
Kwan Yui Chiu <kwan.chiu@kcl.ac.uk>
2022-09-29 00:22:46 +0100

    modify frequencies function to frequencies.py and add .idea directory to ,gitignore

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,15 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for item in items:
+        if not isinstance(item, str):
+            item = str(item)
+        if frequencies.get(item):
+            frequencies[item] += 1
+        else:
+            frequencies[item] = 1
     return frequencies
